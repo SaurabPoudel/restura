8026b25849238785e620e1b2d9d08ec3664d7992
SaurabPoudel <poudelsaurab20@gmail.com>
2024-11-27 12:04:08 +0545

    untrack target

--- target/debug/deps/restura-50cda9319dbbe857.d
+++
@@ -1,5 +0,0 @@
-/home/saurab/dev/rust-stuff/restura/target/debug/deps/librestura-50cda9319dbbe857.rmeta: src/main.rs
-
-/home/saurab/dev/rust-stuff/restura/target/debug/deps/restura-50cda9319dbbe857.d: src/main.rs
-
-src/main.rs:
